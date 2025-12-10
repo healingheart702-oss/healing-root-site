474024b16296404ced24097c1c597a83433e29d8
healingheart702-oss <healingheart702@gmail.com>
2025-12-10 12:02:49 -0600

    Initialize Firebase with configuration
    Add Firebase configuration and initialization code.

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyAgjMFw0dbM7CBH4S_zrmPhE69pp84Tpdo",
+  authDomain: "healing-root-farm.firebaseapp.com",
+  projectId: "healing-root-farm",
+  storageBucket: "healing-root-farm.firebasestorage.app",
+  messagingSenderId: "1042258816994",
+  appId: "1:1042258816994:web:0b6dd6b7f1c370ee7093bb"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
