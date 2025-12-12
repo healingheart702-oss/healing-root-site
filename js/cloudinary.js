a4bbfc7bc15b89a5a4b67b9399bfd66f1fcca0c1
healingheart702-oss <healingheart702@gmail.com>
2025-12-12 13:46:56 +0100

    Delete js directory

--- js/cloudinary.js
+++
@@ -1,6 +0,0 @@
-// js/cloudinary.js
-export const CLOUDINARY = {
-  cloudName: "dd7dre9hd",
-  unsignedPreset: "unsigned_upload",
-  uploadUrl: (cloudName) => `https://api.cloudinary.com/v1_1/${cloudName}/upload`
-};
