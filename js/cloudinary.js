68b086716be17aa26add682eea70626ad78f472c
healingheart702-oss <healingheart702@gmail.com>
2025-12-12 12:26:09 +0100

    Update cloudinary.js

--- js/cloudinary.js
+++ js/cloudinary.js
@@ -1,7 +1,6 @@
 // js/cloudinary.js
 export const CLOUDINARY = {
   cloudName: "dd7dre9hd",
-  apiKey: "436638471543752",
-  unsignedPreset: "unsigned_upload", // your unsigned preset
+  unsignedPreset: "unsigned_upload",
   uploadUrl: (cloudName) => `https://api.cloudinary.com/v1_1/${cloudName}/upload`
 };
